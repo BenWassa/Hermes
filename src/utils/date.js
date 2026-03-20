534703ed14b5e9e143a83f522e978b41542d9588
BenWassa <haddonbenjamin@gmail.com>
2026-03-19 21:57:50 -0400

    chore: remove getTodayId debug logs

--- src/utils/date.js
+++ src/utils/date.js
@@ -3,13 +3,6 @@ export function getTodayId() {
   const year = d.getFullYear();
   const month = String(d.getMonth() + 1).padStart(2, '0');
   const day = String(d.getDate()).padStart(2, '0');
-  const localDateId = `${year}-${month}-${day}`;
   
-  console.log('Hermes getTodayId:', {
-    systemIso: new Date().toISOString().split('T')[0],
-    localDateId: localDateId,
-    timestamp: d.toString()
-  });
-  
-  return localDateId;
+  return `${year}-${month}-${day}`;
 }
